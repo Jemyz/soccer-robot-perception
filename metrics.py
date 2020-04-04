e711a997c3c3b83bffe15f89ff60ae08a6586700
mahmoud <mahmoud.gamal30@yahoo.com>
2020-04-05 00:45:07 +0200

    implements detection metric requirements

--- metrics.py
+++ metrics.py
@@ -28,49 +28,50 @@ def seg_iou(ground_truth, predicted, classes):
     return avg_iou
 
 
-def det_accuracy():
-    return 0.0
-
-
-def det_recall(ground_truth, predicted, classes, tolerance=0.00):
-    avg_recall = 0
+def det_info(ground_truth_centers, predicted_centers, classes, threshold=5):
+    tp = 0
+    fp = 0
+    fn = 0
     for c in classes:
-        gt = np.all(ground_truth == c, axis=-1)
-        pr = np.all(predicted == c, axis=-1)
-        intersection = np.logical_and(gt, pr)
-        recall_score = np.sum(intersection) / (np.sum(gt) + tolerance)
+        gts = ground_truth_centers[np.all(np.array(ground_truth_centers[:, 2].tolist()) == c, axis=-1)]
+        prs = predicted_centers[np.all(np.array(predicted_centers[:, 2].tolist()) == c, axis=-1)]
 
-        avg_recall += recall_score
-    avg_recall = avg_recall / len(classes)
-    return avg_recall
+        from scipy.spatial import KDTree
+
+        tree = KDTree(gts[:, 0].tolist())
+        neighbor_dists, neighbor_indices = tree.query(prs[:, 0].tolist())
+
+        sorted_indices = (np.lexsort((neighbor_dists, neighbor_indices)))
+        correct_indices = [sorted_indices[i] for i in
+                           range(len(sorted_indices)) if
+                           (neighbor_indices[sorted_indices[i]] != neighbor_indices[sorted_indices[i - 1]]
+                            and neighbor_dists[sorted_indices[i]] < threshold) or i == 0]
+
+        tp += len(correct_indices)
+        fp += len(sorted_indices) - len(correct_indices)
+        fn += len(gts) - len(correct_indices)
+    return tp, fp, fn
 
 
-def det_precision(ground_truth, predicted, classes, tolerance=0.00):
-    avg_precision = 0
-    for c in classes:
-        gt = np.all(ground_truth == c, axis=-1)
-        pr = np.all(predicted == c, axis=-1)
-        intersection = np.logical_and(gt, pr)
-        precision_score = np.sum(intersection) / (np.sum(pr) + tolerance)
+def det_accuracy(ground_truth_centers, predicted_centers, classes, threshold=5):
+    tp, fp, fn = det_info(ground_truth_centers, predicted_centers, classes, threshold)
+    return tp / (tp + fp + fn)
 
-        avg_precision += precision_score
-    avg_precision = avg_precision / len(classes)
-    return avg_precision
+
+def det_recall(ground_truth_centers, predicted_centers, classes, threshold=5):
+    tp, fp, fn = det_info(ground_truth_centers, predicted_centers, classes, threshold)
+    return tp / (tp + fn)
+
+
+def det_precision(ground_truth_centers, predicted_centers, classes, threshold=5):
+    tp, fp, fn = det_info(ground_truth_centers, predicted_centers, classes, threshold)
+    return tp / (tp + fp)
 
 
 def det_f1score(precision, recall, tolerance=0.00):
     return (precision * recall) / (precision + recall + tolerance)
 
 
-def det_false_rate(ground_truth, predicted, classes, tolerance=0.00):
-    avg_false_rate = 0
-    for c in classes:
-        gt = np.all(ground_truth == c, axis=-1)
-        pr = np.all(predicted == c, axis=-1)
-        intersection = np.logical_and(gt, pr)
-        union = np.logical_or(gt, pr)
-        false_rate_score = 1 - (np.sum(intersection) / (np.sum(union) + tolerance))
-
-        avg_false_rate += false_rate_score
-    avg_false_rate = avg_false_rate / len(classes)
-    return avg_false_rate
+def det_false_rate(ground_truth_centers, predicted_centers, classes, threshold=5):
+    tp, fp, fn = det_info(ground_truth_centers, predicted_centers, classes, threshold)
+    return 1.0 - tp / (tp + fp + fn)
