aed5ff60204a5bad23493680bbc1d9c70873ebd5
mahmoud <mahmoud.gamal30@yahoo.com>
2020-04-07 00:08:02 +0200

    implements get colored image and get center functions

--- utilities.py
+++ utilities.py
@@ -21,55 +21,93 @@ def getDev():
     return avDev
 
 
-def showImages(img,iter):
-    img = img / 2 + 0.5     # unnormalize
+def showImages(img, iter):
+    img = img / 2 + 0.5  # unnormalize
     img = img.cpu().numpy()
-    img = img.transpose((1,2,0))
+    img = img.transpose((1, 2, 0))
     plt.imshow(img)
     plt.savefig('./outputs/detected/img_input [' + str(iter) + '].png')
     plt.show()
 
 
-def showDetectedImages(image,iter,stringName):
-    image[image<0.5] = 0
-    values,imageClasses = torch.max(image,dim=0)
-    colorMap = [[255,0,0],[0,0,255],[0,255,0],[255,255,255]]
+def get_predected_centers(images):
+    colorMap = [[255, 0, 0], [0, 0, 255], [0, 255, 0], [255, 255, 255]]
+    batch_centers = []
+    for image in images:
+        image[image < 0.5] = 0
+        values, imageClasses = torch.max(image, dim=0)
+        colorMap = np.array(colorMap)
+        imageClasses = np.array(imageClasses)
+
+        indices = (values != 0)
+        centers = np.array(np.argwhere(indices).T)
+        colors = colorMap[imageClasses[indices]]
+        batch_centers.append([centers, colors])
+
+    return batch_centers
+
+
+def get_colored_image(images):
+    detImages = []
+    for image in images:
+        image[image < 0.5] = 0
+        values, imageClasses = torch.max(image, dim=0)
+        colorMap = [[255, 0, 0], [0, 0, 255], [0, 255, 0], [255, 255, 255]]
+        size = imageClasses.size()
+        detImage = np.empty([3, size[0], size[1]])
+        for i in range(size[0]):
+            for j in range(size[1]):
+                if (values[i][j] == 0):
+                    color = colorMap[3]
+                else:
+                    color = colorMap[int(imageClasses[i][j])]
+                for k in range(3):
+                    detImage[k, i, j] = color[k]
+        detImage = detImage.transpose((1, 2, 0))
+        detImages.append(detImage)
+    return np.array(detImages)
+
+
+def showDetectedImages(image, iter, stringName):
+    image[image < 0.5] = 0
+    values, imageClasses = torch.max(image, dim=0)
+    colorMap = [[255, 0, 0], [0, 0, 255], [0, 255, 0], [255, 255, 255]]
     size = imageClasses.size()
-    detImage = np.empty([3,size[0],size[1]])
+    detImage = np.empty([3, size[0], size[1]])
     for i in range(size[0]):
         for j in range(size[1]):
-            if(values[i][j]==0):
+            if (values[i][j] == 0):
                 color = color = colorMap[3]
             else:
                 color = colorMap[int(imageClasses[i][j])]
             for k in range(3):
-                detImage[k,i,j] = color[k]
-    detImage = detImage.transpose((1,2,0))
+                detImage[k, i, j] = color[k]
+    detImage = detImage.transpose((1, 2, 0))
     plt.imshow(detImage)
-    plt.savefig('./outputs/detected/img_'+ stringName+'[' + str(iter) + '].png')
+    plt.savefig('./outputs/detected/img_' + stringName + '[' + str(iter) + '].png')
     plt.show()
 
-def visualiseSegmented(segmentedImage,iter,stringName):
-    colorMap = [[0,0,0],[255,255,255],[0,255,0]]
+
+def visualiseSegmented(segmentedImage, iter, stringName):
+    colorMap = [[0, 0, 0], [255, 255, 255], [0, 255, 0]]
     size = segmentedImage.size()
-    segImage = np.empty([3,size[0],size[1]])
+    segImage = np.empty([3, size[0], size[1]])
     for i in range(size[0]):
         for j in range(size[1]):
             color = colorMap[int(segmentedImage[i][j])]
             for k in range(3):
-                segImage[k,i,j] = color[k]
-    segImage = segImage.transpose((1,2,0))
+                segImage[k, i, j] = color[k]
+    segImage = segImage.transpose((1, 2, 0))
     plt.imshow(segImage)
-    plt.savefig('./outputs/segmented/img_'+ stringName+'[' + str(iter) + '].png')
+    plt.savefig('./outputs/segmented/img_' + stringName + '[' + str(iter) + '].png')
     plt.show()
 
-def plot_learning_curve(loss_errors,task):
-  plt.xlabel("Iterations")
-  plt.ylabel("Loss")
-  plt.plot(np.array(loss_errors))
-  if(task == "detection"):
-      plt.savefig('./outputs/detected/curve.png')
-  else:
-      plt.savefig('./outputs/segmented/curve.png')
-    
-      
+
+def plot_learning_curve(loss_errors, task):
+    plt.xlabel("Iterations")
+    plt.ylabel("Loss")
+    plt.plot(np.array(loss_errors))
+    if (task == "detection"):
+        plt.savefig('./outputs/detected/curve.png')
+    else:
+        plt.savefig('./outputs/segmented/curve.png')
