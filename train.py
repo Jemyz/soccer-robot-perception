a1788b4c772d9172c6508354a8b472ff26473ae4
mahmoud <mahmoud.gamal30@yahoo.com>
2020-04-07 15:29:25 +0200

    adds detection accuracies

--- train.py
+++ train.py
@@ -7,17 +7,18 @@ import numpy as np
 
 import torchvision.transforms as transforms
 import os
-from tvloss import TVLossDetect,TVLossSegment
+from tvloss import TVLossDetect, TVLossSegment
 
-from utilities import getDev, setSeed ,plot_learning_curve
+from utilities import getDev, setSeed, plot_learning_curve
 from operator import add
+
 avDev = getDev()
 print(avDev)
 avDev = "cpu"
 seed = 1
 setSeed(seed)
 
-from utilities import showImages, showDetectedImages,visualiseSegmented
+from utilities import showImages, showDetectedImages, visualiseSegmented
 
 lr = 0.001
 batchSize = 20
@@ -25,21 +26,17 @@ epochs = 100
 tvweightdetection = 0.000001
 tvweightsegmentation = 0.00001
 
-
 from dataloader import CudaVisionDataLoader
-
 
 listTransforms_train = [transforms.ToTensor(), transforms.Normalize(mean=[0.5, 0.5, 0.5],
                                                                     std=[0.5, 0.5, 0.5])]
 listTransforms_test = [transforms.ToTensor(), transforms.Normalize(mean=[0.5, 0.5, 0.5],
                                                                    std=[0.5, 0.5, 0.5])]
 
-
-
 data = CudaVisionDataLoader()
 parentDir = './small_data'
 dirDetectionDataset = os.path.join(parentDir, 'detection')
-dirSegmentationDataset = os.path.join(parentDir , 'segmentation')
+dirSegmentationDataset = os.path.join(parentDir, 'segmentation')
 
 train_loader_detection = data.__call__(os.path.join(dirDetectionDataset, 'train'), "detection", listTransforms_train,
                                        batchSize)
@@ -47,13 +44,16 @@ validate_loader_detection = data.__call__(os.path.join(dirDetectionDataset, 'val
                                           listTransforms_test, batchSize)
 test_loader_detection = data.__call__(os.path.join(dirDetectionDataset, 'test'), "detection", listTransforms_test,
                                       batchSize)
-train_loader_segmentation = data.__call__(os.path.join(dirSegmentationDataset,'train'),"segmentation",listTransforms_train,batchSize)
-validate_loader_segmentation = data.__call__(os.path.join(dirSegmentationDataset,'validate'),"segmentation",listTransforms_test,batchSize)
-test_loader_segmentation = data.__call__(os.path.join(dirSegmentationDataset,'test'),"segmentation",listTransforms_test,batchSize)
-
+train_loader_segmentation = data.__call__(os.path.join(dirSegmentationDataset, 'train'), "segmentation",
+                                          listTransforms_train, batchSize)
+validate_loader_segmentation = data.__call__(os.path.join(dirSegmentationDataset, 'validate'), "segmentation",
+                                             listTransforms_test, batchSize)
+test_loader_segmentation = data.__call__(os.path.join(dirSegmentationDataset, 'test'), "segmentation",
+                                         listTransforms_test, batchSize)
 
 from model import soccerSegment
-from metrics import det_accuracy,segmentationAccuracy,seg_iou
+from metrics import det_metrics, segmentationAccuracy, seg_iou
+from utilities import get_colored_image, get_predected_centers
 
 
 def train():
@@ -72,6 +72,7 @@ def train():
     val_counter = 0
     best_acc = np.NINF
     epoch = 0
+    color_classes = [[255, 0, 0], [0, 0, 255], [0, 255, 0]]
 
     if os.path.exists(checkpoint_path):
         checkpoint = torch.load(checkpoint_path)
@@ -81,26 +82,28 @@ def train():
         segloss = checkpoint['seg_loss']
         detloss = checkpoint['det_loss']
         print("Checkpoint Loaded")
-    detlosses= []
+    detlosses = []
     seglosses = []
     while epoch < epochs:
 
         print("Epoch: ", epoch)
-        train_acc = 0.0
+        det_train_acc = 0.0
+        seg_train_acc = 0.0
+
         model.train()
         steps = 0
         fullDataCovered = 0
         dataiter_detection = train_loader_detection.__iter__()
         dataiter_segmentation = train_loader_segmentation.__iter__()
-        
-        while(fullDataCovered!=1):
+
+        while (fullDataCovered != 1):
             for i in range(2):
                 try:
-                    images, targets,target_center = dataiter_detection.next()
+                    images, targets, target_centers = dataiter_detection.next()
                 except:
                     fullDataCovered = 1
                     dataiter_detection = train_loader_detection.__iter__()
-                    images, targets,target_center = dataiter_detection.next()
+                    images, targets, target_centers = dataiter_detection.next()
                 images = images.to(avDev)
                 targets = targets.to(avDev)
                 optimizer.zero_grad()
@@ -108,90 +111,108 @@ def train():
                 tvLoss = TVLossDetect(tvweightdetection)
                 total_variation_loss = tvLoss.forward(detected)
                 mseloss = criterionDetected(detected, targets)
-                loss =  mseloss + total_variation_loss
+                loss = mseloss + total_variation_loss
                 detlosses.append(loss.item())
                 print("Epoch: ", epoch, "step#: ", steps, " Train detection loss: ", loss.item())
                 steps += 1
-                    # Getting gradients w.r.t. parameters
+                # Getting gradients w.r.t. parameters
                 loss.backward()
-            # Updating parameters
+                # Updating parameters
                 optimizer.step()
-                
-                #train_acc += det_accuracy(detImage,colorMap)
-                i +=1
+
+                ground_truth_centers = get_predected_centers(target_centers)
+                colored_images = get_colored_image(detected)
+                det_train_acc += np.average(det_metrics(ground_truth_centers, colored_images, color_classes)[0])
+
+                i += 1
             try:
-                    images, targets = dataiter_segmentation.next()
+                images, targets = dataiter_segmentation.next()
             except:
-                    fullDataCovered = 1
-                    dataiter_segmentation = train_loader_segmentation.__iter__()
-                    images, targets = dataiter_segmentation.next()
+                fullDataCovered = 1
+                dataiter_segmentation = train_loader_segmentation.__iter__()
+                images, targets = dataiter_segmentation.next()
             images = images.to(avDev)
             targets = targets.to(avDev)
             optimizer.zero_grad()
             segmented, detected = model(images)
-            segmentedLabels=torch.argmax(segmented,dim=1)
-            accuracies =segmentationAccuracy(segmentedLabels.long(),targets,[0,1,2])
+            segmentedLabels = torch.argmax(segmented, dim=1)
+            accuracies = segmentationAccuracy(segmentedLabels.long(), targets, [0, 1, 2])
             tvLoss = TVLossSegment(tvweightsegmentation)
             total_variation_loss = tvLoss.forward(segmented)
             cross_entropy_loss = criterionSegmented(segmented, targets)
-            loss =  cross_entropy_loss + total_variation_loss
+            loss = cross_entropy_loss + total_variation_loss
             seglosses.append(loss.item())
             print("Epoch: ", epoch, "step#: ", steps, " Train segmentation loss: ", loss.item())
             steps += 1
-                    # Getting gradients w.r.t. parameters
+            # Getting gradients w.r.t. parameters
             loss.backward()
             # Updating parameters
             optimizer.step()
-            train_acc += det_accuracy()
-            i +=1
-            
-        train_acc /= len(train_loader_detection)
-        val_acc = 0.0
-        seg_acc = 0.0
+
+            seg_train_acc += accuracies[3]
+
+            i += 1
+
+        det_train_acc /= len(train_loader_detection)
+        seg_train_acc /= len(train_loader_segmentation)
+
+        det_val_acc = 0.0
+        seg_val_acc = 0.0
         model.eval()
         steps = 0
-        
-        for images, targets,target_center in validate_loader_detection:
+
+        for images, targets, target_centers in validate_loader_detection:
             with torch.no_grad():
                 images = images.to(avDev)
                 targets = targets.to(avDev)
-                segmented, detected = model(images)
 
                 segmented, detected = model(images)
                 tvLoss = TVLossDetect(tvweightdetection)
                 total_variation_loss = tvLoss.forward(detected)
                 mseloss = criterionDetected(detected, targets)
-                detloss =  mseloss + total_variation_loss
-                
+                detloss = mseloss + total_variation_loss
+
                 print("Epoch: ", epoch, "step#: ", steps, " Detection Validate loss: ", detloss.item())
                 steps += 1
-                
-        val_acc /= len(validate_loader_detection)
+
+                ground_truth_centers = get_predected_centers(target_centers)
+                colored_images = get_colored_image(detected)
+                det_val_acc += np.average(det_metrics(ground_truth_centers, colored_images, color_classes)[0])
+
         for images, targets in validate_loader_segmentation:
             model.eval()
             with torch.no_grad():
-                         
                 images = images.to(avDev)
                 targets = targets.to(avDev)
 
-                segmented,detected = model(images)
+                segmented, detected = model(images)
 
-                segmentedLabels=torch.argmax(segmented,dim=1)
+                segmentedLabels = torch.argmax(segmented, dim=1)
                 tvLoss = TVLossSegment(tvweightsegmentation)
             total_variation_loss = tvLoss.forward(segmented)
             entropy_loss = criterionSegmented(segmented, targets.long())
             segloss = entropy_loss + total_variation_loss
             print("Epoch: ", epoch, "step#: ", steps, " Segmentation Validate loss: ", loss.item())
-            
-            accuracies =segmentationAccuracy(segmentedLabels.long(),targets,[0,1,2])
-            print('Validate Segmentation Accuracy: Background,Line,Field,Total:',accuracies[0],accuracies[1],accuracies[2],accuracies[3])
-            seg_acc += accuracies[3]
-        seg_acc /=len(validate_loader_segmentation) 
-        acc = (val_acc+seg_acc)/2
+
+            accuracies = segmentationAccuracy(segmentedLabels.long(), targets, [0, 1, 2])
+            print('Validate Segmentation Accuracy: Background,Line,Field,Total:', accuracies[0], accuracies[1],
+                  accuracies[2], accuracies[3])
+            seg_val_acc += accuracies[3]
+
+        det_val_acc /= len(validate_loader_detection)
+        seg_val_acc /= len(validate_loader_segmentation)
+
+        print("Epoch: ", epoch, " Finished")
+        print("Epoch: ", epoch, " Detection Train Accuracy: ", det_train_acc, " Detection Validation Accuracy: ",
+              det_val_acc)
+        print("Epoch: ", epoch, " Segmentation Train Accuracy: ", det_train_acc,
+              " Segmentation Validation Accuracy: ", seg_val_acc)
+
+        acc = (det_val_acc + seg_val_acc) / 2
         if acc > best_acc:
             val_counter = 0
             best_acc = acc
-            print("New Best Validation Accuracy Detection,Segmentation: ", val_acc,seg_acc)
+            print("New Best Validation Accuracy Detection: ", det_val_acc, " Segmentation: ", seg_val_acc)
             torch.save({
                 'epoch': epoch,
                 'model_state_dict': model.state_dict(),
@@ -208,9 +229,10 @@ def train():
         epoch += 1
 
     count = 0
-    plot_learning_curve(detlosses,"detection")
-    plot_learning_curve(seglosses,"segmentation")
-    for images, targets,target_center in test_loader_detection:
+    plot_learning_curve(detlosses, "detection")
+    plot_learning_curve(seglosses, "segmentation")
+    det_test_metric = np.zeros((5, len(color_classes)))
+    for images, targets, target_center in test_loader_detection:
         count += 1
         model.eval()
         with torch.no_grad():
@@ -222,47 +244,79 @@ def train():
             tvLoss = TVLossDetect(tvweightdetection)
             total_variation_loss = tvLoss.forward(detected)
             mseloss = criterionDetected(detected, targets)
-            loss =  mseloss + total_variation_loss
+            loss = mseloss + total_variation_loss
             print("Detection Test loss: ", loss.item())
+            ground_truth_centers = get_predected_centers(target_centers)
+            colored_images = get_colored_image(detected)
+            det_test_metric += det_metrics(ground_truth_centers, colored_images, color_classes)
+
             for j in range(batchSize):
-                showImages(images[j], count+j)
-                showDetectedImages(detected[j], count+j, "output")
-                showDetectedImages(targets[j], count+j, "truth")
-    accuracies = [0,0,0,0]
-    iou = [0,0,0,0]
+                showImages(images[j], count + j)
+                showDetectedImages(detected[j], count + j, "output")
+                showDetectedImages(targets[j], count + j, "truth")
+
+    det_test_metric /= len(test_loader_detection)
+
+    print('Test Detection Overall Accuracy: {}.', np.average(det_test_metric[0]))
+    print('Ball Accuracy:', det_test_metric[0][0])
+    print('Robot Accuracy:', det_test_metric[0][1])
+    print('Goal Pillar Accuracy:', det_test_metric[0][2])
+
+    print('Test Detection Overall Recall: {}.', np.average(det_test_metric[1]))
+    print('Ball Recall:', det_test_metric[1][0])
+    print('Robot Recall:', det_test_metric[1][1])
+    print('Goal Pillar Recall:', det_test_metric[1][2])
+
+    print('Test Detection Overall Precision: {}.', np.average(det_test_metric[2]))
+    print('Ball Precision:', det_test_metric[2][0])
+    print('Robot Precision:', det_test_metric[2][1])
+    print('Goal Pillar Precision:', det_test_metric[2][2])
+
+    print('Test Detection Overall F1score: {}.', np.average(det_test_metric[3]))
+    print('Ball F1score:', det_test_metric[3][0])
+    print('Robot F1score:', det_test_metric[3][1])
+    print('Goal Pillar F1score:', det_test_metric[3][2])
+
+    print('Test Detection Overall False Rate: {}.', np.average(det_test_metric[4]))
+    print('Ball False Rate:', det_test_metric[4][0])
+    print('Robot False Rate:', det_test_metric[4][1])
+    print('Goal Pillar False Rate:', det_test_metric[4][2])
+
+    accuracies = [0, 0, 0, 0]
+    iou = [0, 0, 0, 0]
     for images, targets in test_loader_segmentation:
-        count+=1
+        count += 1
         model.eval()
         with torch.no_grad():
-                         
-          images = images.to(avDev)
-          targets = targets.to(avDev)
 
-          segmented,detected = model(images)
+            images = images.to(avDev)
+            targets = targets.to(avDev)
 
-          segmentedLabels=torch.argmax(segmented,dim=1)
-          tvLoss = TVLossSegment(tvweightsegmentation)
-          total_variation_loss = tvLoss.forward(segmented)
-          entropy_loss = criterionSegmented(segmented, targets.long())
-          loss = entropy_loss + total_variation_loss
-          print("Segmentation Test loss: ", loss.item())
-        accuracies_returned = segmentationAccuracy(segmentedLabels.long(),targets,[0,1,2])
-        iou_returned = seg_iou(targets,segmentedLabels.long(),[0,1,2])
-        accuracies = list( map(add, accuracies, accuracies_returned) )
-        iou = list( map(add, iou, iou_returned) )
+            segmented, detected = model(images)
+
+            segmentedLabels = torch.argmax(segmented, dim=1)
+            tvLoss = TVLossSegment(tvweightsegmentation)
+            total_variation_loss = tvLoss.forward(segmented)
+            entropy_loss = criterionSegmented(segmented, targets.long())
+            loss = entropy_loss + total_variation_loss
+            print("Segmentation Test loss: ", loss.item())
+        accuracies_returned = segmentationAccuracy(segmentedLabels.long(), targets, [0, 1, 2])
+        iou_returned = seg_iou(targets, segmentedLabels.long(), [0, 1, 2])
+        accuracies = list(map(add, accuracies, accuracies_returned))
+        iou = list(map(add, iou, iou_returned))
         for j in range(batchSize):
-            showImages(images[j],count+j)
-            visualiseSegmented(segmentedLabels[j],count+j,"output")
-            visualiseSegmented(targets[j],count+j,"truth")
-        
-    print('Test Segmentation Accuracy: {}.',accuracies[3]/count)
-    print('Field Accuracy:',accuracies[2]/count)
-    print('Line Accuracy:',accuracies[1]/count)
-    print('Background Accuracy:',accuracies[0]/count)
-    print('Iou:' , iou[3]/count)
-    print('Iou Field',iou[2]/count)
-    print('Iou Line',iou[1]/count)
-    print('Iou Background',iou[0]/count)
-    
+            showImages(images[j], count + j)
+            visualiseSegmented(segmentedLabels[j], count + j, "output")
+            visualiseSegmented(targets[j], count + j, "truth")
+
+    print('Test Segmentation Accuracy: {}.', accuracies[3] / count)
+    print('Field Accuracy: ', accuracies[2] / count)
+    print('Line Accuracy: ', accuracies[1] / count)
+    print('Background Accuracy: ', accuracies[0] / count)
+    print('Iou: ', iou[3] / count)
+    print('Iou Field: ', iou[2] / count)
+    print('Iou Line: ', iou[1] / count)
+    print('Iou Background: ', iou[0] / count)
+
 
 train()
