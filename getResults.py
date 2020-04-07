adae7050d06a564a2055feb6c425ed55807a99cc
Sarah0711 <sarahjmi07@gmail.com>
2020-04-07 16:25:08 +0200

    Minor changes for plotting curve and images

--- getResults.py
+++ getResults.py
@@ -15,7 +15,7 @@ avDev = "cpu"
 seed = 1
 setSeed(seed)
 
-from utilities import showImages, showDetectedImages,visualiseSegmented
+from utilities import showImagesDetection,showImagesSegmentation, showDetectedImages,visualiseSegmented
 lr = 0.001
 batchSize = 1
 epochs = 100
@@ -42,7 +42,7 @@ test_loader_segmentation = data.__call__(os.path.join(dirSegmentationDataset,'te
 
 
 from model import soccerSegment
-from metrics import det_accuracy,segmentationAccuracy,seg_iou
+from metrics import det_accuracy,segmentationAccuracy,det_metrics,seg_iou,get_predected_centers,get_colored_image
 import torchvision.models as models
 resnet18 = models.resnet18(pretrained=True)
 model = soccerSegment(resnet18, [5, 6, 7, 8], [64, 128, 256, 256, 0], [512, 256, 256, 128], [512, 512, 256], 256)
@@ -62,10 +62,16 @@ if os.path.exists(checkpoint_path):
     checkpoint = torch.load(checkpoint_path)
     model.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch'] + 1
+    seglosses = checkpoint['seglosses']
+    detlosses = checkpoint['detlosses']
     print("Checkpoint Loaded")
 num =0
-for images, targets,list_center,list_class in test_loader_detection:
-        
+count =0
+color_classes = [[255, 0, 0], [0, 0, 255], [0, 255, 0]]
+det_test_metric = np.zeros((5, len(color_classes)))
+for images, targets, target_center in test_loader_detection:
+    count += 1
     model.eval()
     with torch.no_grad():
         images = images.to(avDev)
@@ -76,13 +82,44 @@ for images, targets,list_center,list_class in test_loader_detection:
         tvLoss = TVLossDetect(tvweightdetection)
         total_variation_loss = tvLoss.forward(detected)
         mseloss = criterionDetected(detected, targets)
-        loss =  mseloss + total_variation_loss
+        loss = mseloss + total_variation_loss
         print("Detection Test loss: ", loss.item())
+        ground_truth_centers = get_predected_centers(target_center)
+        colored_images = get_colored_image(detected)
+        det_test_metric += det_metrics(ground_truth_centers, colored_images, color_classes)
+
         for j in range(batchSize):
             num +=1
-            showImages(images[j], num)
+            showImagesDetection(images[j], num)
             showDetectedImages(detected[j], num, "output")
             showDetectedImages(targets[j], num, "truth")
+
+det_test_metric /= len(test_loader_detection)
+
+print('Test Detection Overall Accuracy: {}.', np.average(det_test_metric[0]))
+print('Ball Accuracy:', det_test_metric[0][0])
+print('Robot Accuracy:', det_test_metric[0][1])
+print('Goal Pillar Accuracy:', det_test_metric[0][2])
+
+print('Test Detection Overall Recall: {}.', np.average(det_test_metric[1]))
+print('Ball Recall:', det_test_metric[1][0])
+print('Robot Recall:', det_test_metric[1][1])
+print('Goal Pillar Recall:', det_test_metric[1][2])
+
+print('Test Detection Overall Precision: {}.', np.average(det_test_metric[2]))
+print('Ball Precision:', det_test_metric[2][0])
+print('Robot Precision:', det_test_metric[2][1])
+print('Goal Pillar Precision:', det_test_metric[2][2])
+
+print('Test Detection Overall F1score: {}.', np.average(det_test_metric[3]))
+print('Ball F1score:', det_test_metric[3][0])
+print('Robot F1score:', det_test_metric[3][1])
+print('Goal Pillar F1score:', det_test_metric[3][2])
+
+print('Test Detection Overall False Rate: {}.', np.average(det_test_metric[4]))
+print('Ball False Rate:', det_test_metric[4][0])
+print('Robot False Rate:', det_test_metric[4][1])
+print('Goal Pillar False Rate:', det_test_metric[4][2])
 accuracies = [0,0,0,0]
 iou = [0,0,0,0]
 count = 0
@@ -109,7 +146,7 @@ for images, targets in test_loader_segmentation:
         iou = list( map(add, iou, iou_returned) )
         for j in range(batchSize):
             num+=1
-            showImages(images[j],num)
+            showImagesSegmentation(images[j],num)
             visualiseSegmented(segmentedLabels[j],num,"output")
             visualiseSegmented(targets[j],num,"truth")
         
